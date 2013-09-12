5aead50a7d14b2b338570ba9af0a90b79b930eed
Andre Medeiros <me@andremedeiros.info>
2013-09-12 11:42:57 +0200

    Create ~/.pow

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,8 @@ class pow {
     ensure   => 'latest',
     provider => 'homebrew'
   }
+
+  file { "/Users/${boxen::config::login}/.pow":
+    ensure => directory
+  }
 }
--- spec/classes/pow_spec.rb
+++ spec/classes/pow_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 describe 'pow' do
   let(:facts) do
     {
-      :boxen_home => '/opt/boxen'
+      :boxen_home   => '/opt/boxen',
+      :boxen_login  => 'testuser'
     }
   end
 
   it do
     should contain_class('pow')
     should contain_package('pow')
+
+    should contain_file("/Users/#{ facts[:boxen_user] }/.pow")
   end
 end
